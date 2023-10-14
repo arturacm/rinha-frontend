e16b3645153a951ce0d45f4dca4e175c1c83feca
Artur Moreira <arturacm@gmail.com>
2023-10-14 11:28:46 -0300

    feat: initial JSON loading screen setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+import styles from "./page.module.css";
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <main className={styles.main}>
+      <h1 className={styles.title}>JSON Tree Viewer</h1>
+      <p className={styles.subTitle}>
+        Simple JSON Viewer that runs completely on-client. No data exchange
+      </p>
+      <button className={styles.loadButton}>Load JSON</button>
+      <span className={styles.error}>Invalid file. Please load a valid JSON file.</span>
+    </main>
   );
 }
