af1fdc6ac8d50ab9ff768345b217a97691c5faaf
Artur Moreira <arturacm@gmail.com>
2023-10-14 13:48:24 -0300

    feat: add simple file loader

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,45 @@
+"use client";
+
+import { ChangeEvent, useCallback, useState } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
+  const [jsonFile, setJsonFile] = useState<File>();
+  const [hasInputError, setInputError] = useState(false);
+
+  const handleFileInput = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+    if (file.type === "application/json") {
+      setJsonFile(file);
+      setInputError(false);
+      console.log(file);
+    } else {
+      setInputError(true);
+    }
+  }, []);
+
   return (
     <main className={styles.main}>
       <h1 className={styles.title}>JSON Tree Viewer</h1>
       <p className={styles.subTitle}>
         Simple JSON Viewer that runs completely on-client. No data exchange
       </p>
-      <button className={styles.loadButton}>Load JSON</button>
-      <span className={styles.error}>Invalid file. Please load a valid JSON file.</span>
+      <label className={styles.loadButton} htmlFor="json-input">
+        Load JSON
+      </label>
+      <input
+        name="json-input"
+        id="json-input"
+        type="file"
+        className={styles.jsonInput}
+        onChange={handleFileInput}
+      />
+      {hasInputError ? (
+        <span className={styles.error}>
+          Invalid file. Please load a valid JSON file.
+        </span>
+      ) : null}
     </main>
   );
 }
