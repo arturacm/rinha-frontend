d4ac459c70bf5f1cee8c46756319ede0392772ca
Artur Moreira <arturacm@gmail.com>
2023-10-24 08:46:54 -0300

    feat: add useOnScreen custom hook
    returns true if the reffed component is on the screen

---
+++ src/utils/useOnScreen.tsx
@@ -0,0 +1,30 @@
+import { useEffect, useState, MutableRefObject } from "react";
+
+const useOnScreen = (ref: MutableRefObject<any>, rootMargin = "0px") => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisible(entry.isIntersecting);
+      },
+      {
+        rootMargin,
+      }
+    );
+
+    const currentElement = ref?.current;
+
+    if (currentElement) {
+      observer.observe(currentElement);
+    }
+
+    return () => {
+      observer.unobserve(currentElement);
+    };
+  }, [ref, rootMargin]);
+
+  return isVisible;
+};
+
+export default useOnScreen;
